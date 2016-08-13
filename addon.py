868e274cb1cbc4e7dc72f8cd9262e69736958bed
Jonathan Holvey <jonathan.holvey@outlook.com>
2016-08-13 13:41:45 +1000

    Add basic parser to list radio sources from sources.xml
    With title, artwork and tagline

---
+++ addon.py
@@ -0,0 +1,27 @@
+import os
+import xbmcgui
+import xbmcaddon
+import xbmcplugin
+import xml.etree.ElementTree as et
+
+handle = int(sys.argv[1])
+xbmcplugin.setContent(handle, "audio")
+
+addon = xbmcaddon.Addon(id="plugin.audio.streaming-radio")
+
+sources = et.parse(os.path.join(addon.getAddonInfo("path"), "sources.xml"))
+
+for source in sources.iter("radio"):
+	url = source.find("stream").text
+	li = xbmcgui.ListItem(source.find("name").text, iconImage="DefaultAudio.png")
+	li.setInfo("music", {
+		"title": source.find("name").text,
+		"artist": source.find("tagline").text
+	})
+	li.setArt({
+		"thumb": os.path.join(addon.getAddonInfo("path"), "artwork", source.find("thumbnail").text),
+		"fanart": os.path.join(addon.getAddonInfo("path"), "artwork", source.find("fanart").text)
+	})
+	xbmcplugin.addDirectoryItem(handle=handle, url=url, listitem=li)
+
+xbmcplugin.endOfDirectory(handle)
