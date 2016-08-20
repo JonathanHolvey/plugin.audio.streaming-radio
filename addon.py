8a069bdd1cc755a74345a5c25223d1ce8601009b
Jonathan Holvey <jonathan.holvey@outlook.com>
2016-08-20 10:52:22 +1000

    Improve compliance with PEP 8 style guide

--- addon.py
+++ addon.py
@@ -1,8 +1,9 @@
 import os
+import xml.etree.ElementTree as et
+
 import xbmcgui
 import xbmcaddon
 import xbmcplugin
-import xml.etree.ElementTree as et
 
 handle = int(sys.argv[1])
 xbmcplugin.setContent(handle, "audio")
@@ -10,18 +11,18 @@ xbmcplugin.setContent(handle, "audio")
 addon = xbmcaddon.Addon(id="plugin.audio.streaming-radio")
 
 sources = et.parse(os.path.join(addon.getAddonInfo("path"), "sources.xml"))
-
+# Loop through sources XML and create list item for each entry
 for source in sources.iter("radio"):
-	url = source.find("stream").text
-	li = xbmcgui.ListItem(source.find("name").text, iconImage="DefaultAudio.png")
-	li.setInfo("music", {
-		"title": source.find("name").text,
-		"artist": source.find("tagline").text
-	})
-	li.setArt({
-		"thumb": os.path.join(addon.getAddonInfo("path"), "artwork", source.find("thumbnail").text),
-		"fanart": os.path.join(addon.getAddonInfo("path"), "artwork", source.find("fanart").text)
-	})
-	xbmcplugin.addDirectoryItem(handle=handle, url=url, listitem=li)
+    url = source.find("stream").text
+    li = xbmcgui.ListItem(source.find("name").text, iconImage="DefaultAudio.png")
+    li.setInfo("music", {
+        "title": source.find("name").text,
+        "artist": source.find("tagline").text
+    })
+    li.setArt({
+        "thumb": os.path.join(addon.getAddonInfo("path"), "artwork", source.find("thumbnail").text),
+        "fanart": os.path.join(addon.getAddonInfo("path"), "artwork", source.find("fanart").text)
+    })
+    xbmcplugin.addDirectoryItem(handle=handle, url=url, listitem=li)
 
 xbmcplugin.endOfDirectory(handle)
