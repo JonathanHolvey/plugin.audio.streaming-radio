f6a668cc68a3e47c5cb62e14cf7e78a78f7269a7
Jonathan Holvey <jonathan.holvey@outlook.com>
2016-08-20 12:22:22 +1000

    Restructure plugin using xbmcplugin.setResolvedUrl() call

--- addon.py
+++ addon.py
@@ -1,19 +1,16 @@
 import os
 import xml.etree.ElementTree as et
+import urlparse
 
 import xbmcgui
 import xbmcaddon
 import xbmcplugin
 
+plugin = sys.argv[0]
 handle = int(sys.argv[1])
-xbmcplugin.setContent(handle, "audio")
-
 addon = xbmcaddon.Addon(id="plugin.audio.streaming-radio")
 
-sources = et.parse(os.path.join(addon.getAddonInfo("path"), "sources.xml"))
-# Loop through sources XML and create list item for each entry
-for source in sources.iter("radio"):
-    url = source.find("stream").text
+def create_list_item(source):
     li = xbmcgui.ListItem(source.find("name").text, iconImage="DefaultAudio.png")
     li.setInfo("music", {
         "title": source.find("name").text,
@@ -23,6 +20,35 @@ for source in sources.iter("radio"):
         "thumb": os.path.join(addon.getAddonInfo("path"), "artwork", source.find("thumbnail").text),
         "fanart": os.path.join(addon.getAddonInfo("path"), "artwork", source.find("fanart").text)
     })
-    xbmcplugin.addDirectoryItem(handle=handle, url=url, listitem=li)
+    return li
 
-xbmcplugin.endOfDirectory(handle)
+
+def build_list():
+    xbmcplugin.setContent(handle, "audio")
+    # Loop through sources XML and create list item for each entry
+    for source in sources.iter("radio"):
+        li = create_list_item(source)
+        li.setProperty("IsPlayable", "true")
+        url = "{}?stream={}".format(plugin, source.find("stream").text)
+        xbmcplugin.addDirectoryItem(handle=handle, url=url, listitem=li, isFolder=False)
+
+    xbmcplugin.endOfDirectory(handle)
+
+
+def play_stream(url):
+    # Loop through sources XML to find entry with specified stream url
+    for source in sources.iter("radio"):
+        if url == source.find("stream").text:
+            break
+    li = create_list_item(source)
+    li.setPath(url)
+    xbmcplugin.setResolvedUrl(handle, True, li)
+
+
+sources = et.parse(os.path.join(addon.getAddonInfo("path"), "sources.xml"))
+params = urlparse.parse_qs(sys.argv[2][1:])
+
+if params.get("stream", None) is None:
+    build_list()
+else:
+    play_stream(params["stream"][0])
