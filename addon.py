cdf7e1abeeec57390eab2151169d7e6481f95f95
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-08-22 20:50:05 +1000

    Update tunein.com scraper regex
    Fixes #12

--- addon.py
+++ addon.py
@@ -186,7 +186,7 @@ class RadioInfo():
     def _update_tunein(self):
         try:
             html = requests.get(self.scraper["url"]).text
-            match = re.search(r"<h3 class=\"title\">(.+?) - (.+?)</h3>", html)
+            match = re.search(r"<p class=\"default__titleOffset.+?>(.+?) - (.+?)</p>", html)
             if match is not None:
                 self.info["artist"] = unescape(match.group(1))
                 self.info["title"] = unescape(match.group(2))
